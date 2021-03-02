d3de66ce96a971a6f4cb37572f2967e10e608aee
Dane Walker <dcw6205@gmail.com>
2021-03-02 20:13:25 +1000

    First Commit 🚀

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EditablePage from './components/editablePage';
+
+function App() {
+  return (
+    <div className="App">
+      <EditablePage></EditablePage>
+    </div>
+  );
+}
+
+export default App;
