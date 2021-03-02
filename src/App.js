4652c12a0738be8d45c20e2f23e762a4dfce3e0b
Dane Walker <dcw6205@gmail.com>
2021-03-02 20:27:31 +1000

    Added App Name to Main Screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EditablePage from './components/editablePage';
 function App() {
   return (
     <div className="App">
+      <h1 className="TitleBlock">Blocky Notes</h1>
       <EditablePage></EditablePage>
     </div>
   );
